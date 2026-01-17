48f6aabf04a77883d4a641b83ebcd6a9da173ffc
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-01-17 15:12:09 +0700

    A_Way_Too_Long_Words.cpp

---
+++ main3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    float f;
+    cin >> f;
+    cout << fixed << setprecision(2) << f << endl;
+    cout << fixed << setprecision(4) << f << endl;
+    cout << fixed << setprecision(6) << f << endl;
+    return 0;
+}
