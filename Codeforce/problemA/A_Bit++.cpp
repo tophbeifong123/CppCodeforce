57d877c6c832a19d482f0b7e84491ee7cc091d37
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-01-28 22:55:56 +0700

    feat(problemA): A_Bit++

---
+++ Codeforce/problemA/A_Bit++.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int count = 0;
+    for(int i=0;i<n;i++){
+        string s; cin >> s;
+        if(s[1] == '+') count++;
+        else count--;
+    }
+    cout << count << "\n";
+    
+    return 0;
+}
