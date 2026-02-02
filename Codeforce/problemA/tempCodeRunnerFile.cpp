31b00258a67140c66673a2609f849a1d0ff43799
Thummeena Pengchai <thrrmunaphengchay@gmail.com>
2026-02-02 15:03:54 +0700

    feat(problemA): A_Elephant cpp

--- Codeforce/problemA/tempCodeRunnerFile.cpp
+++ Codeforce/problemA/tempCodeRunnerFile.cpp
@@ -1,17 +1,2 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int a,b ; cin >> a >> b;
-    int year = 0;
-    if (a == b) {
-        cout << 1;
-    } else{
-        while(b>a){
-            a *= 3;
-            b *= 2;
-            year += 1;
-        }   
-        cout << year;
+
     }
-    return 0;
-}
